62705d9a65331c01ef1147cf75ad6bd7d6ddb252
Isai Tamil Records <67947636+isaitamilrecords@users.noreply.github.com>
2021-07-14 16:58:24 +0530

    Add files via upload

--- scripts/smtp.js
+++ scripts/smtp.js
@@ -2,7 +2,7 @@ function sendMail(sub, message) {
     Email.send({
         SecureToken: "69614780-f300-45d5-96fa-f08905420c15",
         To: "isaitamilrecords@gmail.com",
-        From: "isaitamilrecordswebsitebot@gmail.com",
+        From: "abishekmahi@yahoo.com",
         Subject: sub,
         Body: message
     }).then(
